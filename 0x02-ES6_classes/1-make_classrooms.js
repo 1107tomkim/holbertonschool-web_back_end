54f1fb7afdd47df08f654c0c62581ec0013f83d8
1107tomkim <cryptotaekim@gmail.com>
2022-02-08 02:44:05 +0000

    importing classroom from task 0 and implement a func name initalizerooms that returns array of 3 classroom obj

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  // Return array of 3 classroom objects with sizes
+
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}

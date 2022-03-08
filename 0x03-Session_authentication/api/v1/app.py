e781134a78d3556603aff31b0ab0853d8939c0f1
1107tomkim <cryptotaekim@gmail.com>
2022-03-08 10:57:28 +0000

    Updated app.py

--- 0x03-Session_authentication/api/v1/app.py
+++ 0x03-Session_authentication/api/v1/app.py
@@ -52,12 +52,14 @@ def forbidden(error) -> str:
 def before_request():
     """ before request
     """
-    at = ["/api/v1/status/", "/api/v1/unauthorized", "/api/v1/forbidden"]
+    at = ["/api/v1/status/", "/api/v1/unauthorized", "/api/v1/forbidden"
+          "/api/v1/auth_session/login/"]
     if auth is None:
         return
     if not auth.require_auth(request.path, at):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None\
+       and auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)

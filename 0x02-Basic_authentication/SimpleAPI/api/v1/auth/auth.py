107c1a253e9e82037e2e3e53b1116b0a360c626a
1107tomkim <cryptotaekim@gmail.com>
2022-03-08 03:42:05 +0000

    Adding everything so far

---
+++ 0x02-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Class Auth"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        def method require auth that returns False - path
+        and exculuded_paths
+        """
+        if path is None or excluded_paths is None:
+            return (True)
+        if path in excluded_paths or f"{path}/" in excluded_paths:
+            return(False)
+        return (True)
+
+    def authorization_header(self, request=None) -> str:
+        """
+        def method auth header that returns None - request
+        """
+        if request is None or request.headers.get('Authorization') is None:
+            return (None)
+        return (request.headers.get('Authorization'))
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        def method current user that returns None - request
+        """
+        return (None)

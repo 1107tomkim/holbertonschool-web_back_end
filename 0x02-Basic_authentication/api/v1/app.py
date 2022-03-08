230dbf37c1c87fc2f92b2d808b25cca30d0d6bb8
1107tomkim <cryptotaekim@gmail.com>
2022-03-08 05:28:03 +0000

    Changing if statement to impact Auth Header

--- 0x02-Basic_authentication/api/v1/app.py
+++ 0x02-Basic_authentication/api/v1/app.py
@@ -47,7 +47,7 @@ def before_request():
     """ before request
     """
     at = ["/api/v1/status/", "/api/v1/unauthorized", "/api/v1/forbidden"]
-    if auth and auth.require_auth(request.path, at):
+    if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)

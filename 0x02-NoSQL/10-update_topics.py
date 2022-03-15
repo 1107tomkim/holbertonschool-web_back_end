217a067a591151431b3b12aa2eba1fc44c7e707a
1107tomkim <cryptotaekim@gmail.com>
2022-03-15 03:03:12 +0000

    New format method of updating

--- 0x02-NoSQL/10-update_topics.py
+++ 0x02-NoSQL/10-update_topics.py
@@ -5,10 +5,4 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """Func that changes topics of school doc based on name"""
-    if mongo_collection is None:
-        return []
-    else:
-        return mongo_collection.update_many(
-            {'name': name},
-            {'$set': {'topics': topics}}
-        )
+    return mongo_collection.update_many({"name": name}, { "$set": { "topics": topics } })

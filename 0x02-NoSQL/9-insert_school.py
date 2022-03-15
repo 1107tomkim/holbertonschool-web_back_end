61bcce71129f6e56aa5774fe7c64d252861c807a
1107tomkim <cryptotaekim@gmail.com>
2022-03-15 02:08:32 +0000

    Python func that inserts a new doc in collection

---
+++ 0x02-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts new doc in collection based kwargs"""
+    if mongo_collection is None:
+        return []
+    else:
+        return mongo_collection.insert_one(kwargs).inserted_id
